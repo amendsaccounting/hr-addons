32423a3814c589a1a088aafc072fc9d8454a5ba8
Nijin Joy <nijinjoy1999@gmail.com>
2025-12-05 21:59:41 +0400

    splash

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Image, ActivityIndicator, StyleSheet, StatusBar } from 'react-native';
+import { colors } from '../styles/theme';
 
-const SplashScreen = () => {
+// Use require() to avoid default export interop on RN assets
+const logo = require('../assets/images/logo/logo.png');
+
+export default function SplashScreen() {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.background} />
+      <View style={styles.center}>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
+        <ActivityIndicator color={colors.white} style={{ marginTop: 16 }} />
+      </View>
     </View>
-  )
+  );
 }
 
-export default SplashScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+  },
+});
